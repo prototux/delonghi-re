e3c8960705f0c352a1403cd128948ea1062fcf2e
prototux <jason@prototux.net>
2023-11-04 15:24:37 +0100

    add more vars

--- ecam23.450_displayboard_reverse/reimplem/vars_bank0.h
+++ ecam23.450_displayboard_reverse/reimplem/vars_bank0.h
@@ -1,19 +1,19 @@
 // Starts at 0x20
 // 0x20
 // 0x21
-// 0x22
-// 0x23
-// 0x24
-// 0x25
-// 0x26
+char MLOGIC_SUB1_UNK1; // 0x22
+char TIMER1_UNK5; // 0x23
+char CECILE_UNK1; // 0x24
+char SSP_PACKET_DATA_12_SRC; // 0x25
+char SSP_PACKET_DATA_18_MASK_SRC; // 0x26
 // 0x27
 // 0x28
-// 0x29
+char PUSH_UNK1; // 0x29
 char USART_LOGIC_UNK9; // 0x2a
 char USART_LOGIC_UNK10; // 0x2b
 char ENTRY_DATA_UNK3; // 0x2c
 char USART_SSP_PACKET_STATUS; // 0x2d
-// 0x2e
+char SSP_PACKET_DATA_18_BITCOND; // 0x2e
 char ENTRY_DATA_UNK1; // 0x2f
 char TIMER0_INT_UNK2; // 0x30
 // 0x31
@@ -21,37 +21,44 @@ char INIT_HW1_UNK2; // 0x32
 char INIT_HW1_UNK4; // 0x33
 char INIT_HW1_UNK3; // 0x34
 char INIT_HW1_UNK5; // 0x35
-char TIMER1_UNK2; // 0x34
-// 0x35
-// 0x36
+char TIMER1_UNK2; // 0x36
 char USART_LOGIC_UNK8; // 0x37
 char USART_LOGIC_UNK12; // 0x38
 char USART_LOGIC_UNK11; // 0x39
-// 0x3a
+char TIMER1_UNK4; // 0x3a
 char USART_LOGIC_UNK6; // 0x3b
 char USART_LOGIC_UNK7; // 0x3c
 char TIMER0_INT_UNK3; // 0x3d
 char SSP_PACKET_UNK1; // 0x3e
 char INIT_HW1_UNK1; // 0x3f
-
 char SSP_TX_BUFFER_INDEX; // 0x40
 char SSP_PACKET_SIZE; // 0x41
 
 char SSP_UNK2; // 0x44
 char TIMER0_INT_UNK1; // 0x45
 
-char USART_LOGIC_UNK30; // 0x4a
+char ELONA_UNK1; // 0x47
+char ELONA_UNK2; // 0x48
 
+char USART_LOGIC_UNK30; // 0x4a
+char USART_PACKET_CHECKSUM; // 0x4b
 char USART_LOGIC_UNK2; // 0x4c
 char USART_LOGIC_UNK4; // 0x4d
 char USART_LOGIC_UNK5; // 0x4e
-
+char SSP_PACKET_DATA_12; // 0x4f
+char SSP_PACKET_DATA_18_MASK; // 0x50
+char SSP_PACKET_DATA_18_TMP; // 0x51
+char INIT_ST1_UNK1; // 0x52
+char INIT_ST1_UNK2; // 0x53
+char INIT_ST1_UNK3; // 0x54
 char CHECKSUM_UNK1; // 0x55
 char CHECKSUM_UNK2; // 0x56
+char ADELE_UNK1; // 0x57
 
 char USART_LOGIC_UNK27; // 0x5c
 char USART_LOGIC_UNK28; // 0x5d
 char USART_LOGIC_UNK29; // 0x5e
+char CAROLINE_UNK1; // 0x5f
 
 char USART_RX_RCVD_BYTE; // 0x65
 
@@ -74,5 +81,6 @@ char ISR_BKP_SHWD5; // 0x76
 
 char ISR_BKP_WREG; // 0x78
 char ENTRY_SUB2_UNK1; // 0x79
+char EVELISE_UNK1; // 0x7a
 
 // Ends at 0x7f
